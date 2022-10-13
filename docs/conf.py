92f4a6fd01a5b50c7683ab5dd127d183c9ec3fe9
Chrissy <cmf21@st-andrews.ac.uk>
2022-10-13 11:37:16 +0100

    update doc theme

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,7 @@ autodoc_typehints = "description"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nature'
+html_theme = 'python_docs_theme'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
